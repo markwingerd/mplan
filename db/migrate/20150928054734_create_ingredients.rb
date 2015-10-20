e3173978c1ebfe3857d28f55a17700660f8a189a
markwingerd <mawingerd@gmail.com>
2015-10-20 00:09:57 -0700

    Add initial attributes to migration file.

--- db/migrate/20150928054734_create_ingredients.rb
+++ db/migrate/20150928054734_create_ingredients.rb
@@ -2,6 +2,19 @@ class CreateIngredients < ActiveRecord::Migration
   def change
     create_table :ingredients do |t|
     	t.string :name
+
+ 		# Lifestyles
+    	t.boolean :vegitarian, :default => false
+    	t.boolean :vegan, :default => false
+
+    	# Allergens
+    	t.boolean :lactoseFree, :default => false
+    	t.boolean :glutenFree, :default => false
+    	t.boolean :peanutFree, :default => false
+    	t.boolean :soyFree, :default => false
+
+    	# Purchase properties
+    	t.boolean :buyInWholeUnites, :default => false
     	
       t.timestamps null: false
     end
