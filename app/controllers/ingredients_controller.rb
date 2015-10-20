335fdf9fd203435f06e543cc7f8bc7d32bbd9303
markwingerd <mawingerd@gmail.com>
2015-10-20 03:41:51 -0700

    Altered controller to prove that mass update form will work. WIP

--- app/controllers/ingredients_controller.rb
+++ app/controllers/ingredients_controller.rb
@@ -5,7 +5,12 @@ class IngredientsController < ApplicationController
 	end
 
 	def update_multiple
-		render plain: params
+		ingredient_ids = params[:ingredient].keys
+		@ingredients = Ingredient.find(ingredient_ids)
+		@ingredients.each do |ingredient|
+			ingredient.update_attributes!(:vegitarian => params[:ingredient][ingredient.id.to_s] )
+		end
+		render plain: params[:ingredient]["1"]
 	end
 
 end
