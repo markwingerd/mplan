9fc528808fd3da6f0f903526e73c27024043298c
markwingerd <mawingerd@gmail.com>
2015-10-22 01:18:38 -0700

    Improved controller. Added params handling.

--- app/controllers/ingredients_controller.rb
+++ app/controllers/ingredients_controller.rb
@@ -8,16 +8,20 @@ class IngredientsController < ApplicationController
 		ingredient_ids = params[:ingredient][:ingredient].keys
 		@ingredients = Ingredient.find(ingredient_ids)
 		@ingredients.each do |ingredient|
-			ingredient.update_attributes!({
-				:vegitarian => params[:ingredient][:ingredient][ingredient.id.to_s][:vegitarian],
-				:vegan => params[:ingredient][:ingredient][ingredient.id.to_s][:vegan],
-				:lactoseFree => params[:ingredient][:ingredient][ingredient.id.to_s][:lactoseFree],
-				:glutenFree => params[:ingredient][:ingredient][ingredient.id.to_s][:glutenFree],
-				:buyInWholeUnits => params[:ingredient][:ingredient][ingredient.id.to_s][:buyInWholeUnits],
-				})
-		end
+			ingredient.update_attributes!(ingredient_params(ingredient.id))
+		end	
 		
 		redirect_to :action => 'index'
 	end
 
+	private
+
+		def ingredient_params(id)
+			params.require(:ingredient).require(:ingredient).require(id.to_s).permit(
+				:vegitarian, 
+				:vegan, 
+				:lactoseFree, 
+				:glutenFree, 
+				:buyInWholeUnits)
+		end
 end
