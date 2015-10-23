eba184c1d550a15546e963945d3c4ed068f155ab
markwingerd <mawingerd@gmail.com>
2015-10-22 22:00:40 -0700

    Modified controller to automatically check vegitarian when vegan is set true

--- app/controllers/ingredients_controller.rb
+++ app/controllers/ingredients_controller.rb
@@ -6,9 +6,13 @@ class IngredientsController < ApplicationController
 
 	def update_multiple
 		ingredient_ids = params[:ingredient][:ingredient].keys
-		@ingredients = Ingredient.find(ingredient_ids)
-		@ingredients.each do |ingredient|
-			ingredient.update_attributes!(ingredient_params(ingredient.id))
+		ingredients = Ingredient.find(ingredient_ids)
+		ingredients.each do |ingredient|
+			ingredient_params = ingredient_params(ingredient.id)
+			if ingredient_params[:vegan] == "1"
+				ingredient_params[:vegitarian] = true
+			end
+			ingredient.update_attributes!(ingredient_params)
 		end	
 		
 		redirect_to :action => 'index'
