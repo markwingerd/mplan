ad024099cf4909bdda0cebcff2f1d07cc5bc4c66
markwingerd <mawingerd@gmail.com>
2015-10-27 21:20:06 -0700

    Updated ingredients mvc to use new property model. The controller currently uses a quick fix to use params. This will need to be updated soon.

--- app/controllers/ingredients_controller.rb
+++ app/controllers/ingredients_controller.rb
@@ -5,16 +5,44 @@ class IngredientsController < ApplicationController
 	end
 
 	def update_multiple
+		#params = {"utf8"=>"✓", "_method"=>"put", "authenticity_token"=>"Ch3KLqHoaBwC9iYzomt8+/lLbjBbirwBWY3XUM5ATPEgcO5FqymkY1whgzypdv9/dJic0R+eqaeWsAsTe0wmjw==", :ingredient=>{:ingredient=>{"1"=>{"property_attributes"=>{"vegitarian"=>"1", "vegan"=>"1", "lactoseFree"=>"1", "glutenFree"=>"1"}, "buyInWholeUnits"=>"1"}, "2"=>{"property_attributes"=>{"vegitarian"=>"1", "vegan"=>"1", "lactoseFree"=>"1", "glutenFree"=>"1"}, "buyInWholeUnits"=>"1"}, "3"=>{"property_attributes"=>{"vegitarian"=>"0", "vegan"=>"1", "lactoseFree"=>"1", "glutenFree"=>"1"}, "buyInWholeUnits"=>"1"}, "4"=>{"property_attributes"=>{"vegitarian"=>"1", "vegan"=>"1", "lactoseFree"=>"1", "glutenFree"=>"0"}, "buyInWholeUnits"=>"1"}, "5"=>{"property_attributes"=>{"vegitarian"=>"1", "vegan"=>"1", "lactoseFree"=>"1", "glutenFree"=>"1"}, "buyInWholeUnits"=>"1"}, "6"=>{"property_attributes"=>{"vegitarian"=>"0", "vegan"=>"1", "lactoseFree"=>"1", "glutenFree"=>"1"}, "buyInWholeUnits"=>"1"}, "7"=>{"property_attributes"=>{"vegitarian"=>"0", "vegan"=>"1", "lactoseFree"=>"1", "glutenFree"=>"1"}, "buyInWholeUnits"=>"0"}, "8"=>{"property_attributes"=>{"vegitarian"=>"0", "vegan"=>"1", "lactoseFree"=>"1", "glutenFree"=>"1"}, "buyInWholeUnits"=>"1"}, "9"=>{"property_attributes"=>{"vegitarian"=>"0", "vegan"=>"1", "lactoseFree"=>"1", "glutenFree"=>"1"}, "buyInWholeUnits"=>"0"}, "10"=>{"property_attributes"=>{"vegitarian"=>"0", "vegan"=>"1", "lactoseFree"=>"1", "glutenFree"=>"0"}, "buyInWholeUnits"=>"1"}, "11"=>{"property_attributes"=>{"vegitarian"=>"1", "vegan"=>"0", "lactoseFree"=>"1", "glutenFree"=>"0"}, "buyInWholeUnits"=>"0"}, "12"=>{"property_attributes"=>{"vegitarian"=>"1", "vegan"=>"0", "lactoseFree"=>"0", "glutenFree"=>"1"}, "buyInWholeUnits"=>"1"}, "13"=>{"property_attributes"=>{"vegitarian"=>"0", "vegan"=>"1", "lactoseFree"=>"1", "glutenFree"=>"1"}, "buyInWholeUnits"=>"1"}}}, "commit"=>"Submit", "controller"=>"ingredients", "action"=>"update_multiple"}
+		#render plain: params[:ingredient][:ingredient]
+		#test = params[:ingredient][:ingredient]
+		# test["1"]["property_attributes"] = test["1"]["property"]
+		# test["1"].delete("property")
+		params[:ingredient][:ingredient].each do |k, v|
+			params[:ingredient][:ingredient][k]["property_attributes"] = params[:ingredient][:ingredient][k]["property"]#.delete("property")
+			params[:ingredient][:ingredient][k].delete("property")
+		end
+
 		ingredient_ids = params[:ingredient][:ingredient].keys
 		ingredients = Ingredient.find(ingredient_ids)
 		ingredients.each do |ingredient|
 			ingredient_params = ingredient_params(ingredient.id)
-			if ingredient_params[:vegan] == "1"
-				ingredient_params[:vegitarian] = true
-			end
+		 	# if ingredient_params[:vegan] == "1"
+		 	# 	ingredient_params[:vegitarian] = true
+		 	# end
 			ingredient.update_attributes!(ingredient_params)
 		end	
+		#render plain: params
 		
+		redirect_to :action => 'index'
+	end
+
+	def dfsfdsupdate_multiple
+		params = {:buyInWholeUnits=>false,
+				  :property_attributes=>
+					{:vegitarian=>false, 
+					 :vegan=>true, 
+					 :lactoseFree=>true, 
+					 :glutenFree=>true}
+				 }
+
+		ing = Ingredient.find(1)
+		ing.update(params)
+
+		#render plain: ing.buyInWholeUnits
+
 		redirect_to :action => 'index'
 	end
 
@@ -22,10 +50,11 @@ class IngredientsController < ApplicationController
 
 		def ingredient_params(id)
 			params.require(:ingredient).require(:ingredient).require(id.to_s).permit(
-				:vegitarian, 
-				:vegan, 
-				:lactoseFree, 
-				:glutenFree, 
-				:buyInWholeUnits)
+				:buyInWholeUnits,
+				:property_attributes => [
+					:vegitarian, 
+					:vegan, 
+					:lactoseFree, 
+					:glutenFree ])
 		end
 end
--- app/models/ingredient.rb
+++ app/models/ingredient.rb
@@ -5,4 +5,7 @@ class Ingredient < ActiveRecord::Base
 	has_many :recipes, through: :quantities
 
 	has_one :property
+
+	accepts_nested_attributes_for :property,
+           :allow_destroy => true
 end
