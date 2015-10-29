c68d9a96ce3de18815a530d8d959a5d519fb9b6c
markwingerd <mawingerd@gmail.com>
2015-10-28 17:24:20 -0700

    Updated view to submit ingredient params more cleanly. Refactored controller code.

--- app/controllers/ingredients_controller.rb
+++ app/controllers/ingredients_controller.rb
@@ -5,20 +5,17 @@ class IngredientsController < ApplicationController
 	end
 
 	def update_multiple
-		# Quick Fix: View uses a hash key of property but we need property_attributes.
-		# 			 This manually changes the key.
-		params[:ingredient][:ingredient].each do |k, v|
-			params[:ingredient][:ingredient][k]["property_attributes"] = params[:ingredient][:ingredient][k]["property"]#.delete("property")
-			params[:ingredient][:ingredient][k].delete("property")
-		end
+		#render plain: JSON.pretty_generate(params)
 
 		ingredient_ids = params[:ingredient][:ingredient].keys
 		ingredients = Ingredient.find(ingredient_ids)
 		ingredients.each do |ingredient|
 			ing_params = ingredient_params(ingredient.id)
-		 	if ing_params[:property_attributes][:vegan] == "1"
-		 		ing_params[:property_attributes][:vegitarian] = true
-		 	end
+			if ing_params.has_key?(:property_attributes)
+			 	if ing_params[:property_attributes][:vegan] == "1"
+			 		ing_params[:property_attributes][:vegitarian] = true
+			 	end
+			end
 			ingredient.update_attributes!(ing_params)
 		end	
 		
