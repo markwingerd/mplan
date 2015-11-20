d9f85027c1d4bf07df2b5b70ce09cc437c6489b6
markwingerd <mawingerd@gmail.com>
2015-11-20 00:34:02 -0800

    Fixed bug in association. Updated user.rb to fix a bug with calling all recipes authored. Fixed seed file to include recipe bugfix.

--- app/models/recipe.rb
+++ app/models/recipe.rb
@@ -10,7 +10,7 @@ class Recipe < ActiveRecord::Base
   has_many :ingredients,
            :through => :quantities
 
-  has_one :author, :class_name => 'User', foreign_key: 'id'
+  belongs_to :author, :class_name => 'User'
   has_one :property
 
   accepts_nested_attributes_for :quantities,
