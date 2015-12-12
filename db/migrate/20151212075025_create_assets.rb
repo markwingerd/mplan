81348a6d19f37b7941dc0765e5f248845646f28a
markwingerd <mawingerd@gmail.com>
2015-12-12 00:06:32 -0800

    Updated migration file to include image column.

--- db/migrate/20151212075025_create_assets.rb
+++ db/migrate/20151212075025_create_assets.rb
@@ -1,7 +1,8 @@
 class CreateAssets < ActiveRecord::Migration
   def change
     create_table :assets do |t|
-
+    	t.has_attached_file :image
+    	
       t.timestamps null: false
     end
   end
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,16 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20151027234806) do
+ActiveRecord::Schema.define(version: 20151212075025) do
+
+  create_table "assets", force: :cascade do |t|
+    t.string   "image_file_name"
+    t.string   "image_content_type"
+    t.integer  "image_file_size"
+    t.datetime "image_updated_at"
+    t.datetime "created_at",         null: false
+    t.datetime "updated_at",         null: false
+  end
 
   create_table "ingredients", force: :cascade do |t|
     t.string   "name"
