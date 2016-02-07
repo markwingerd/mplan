cb9b8c93fd2269e6cde33e23e0cedb39e86f8610
Mark Wingerd <mawingerd@gmail.com>
2016-02-06 19:36:35 -0800

    Changed images to save as squares.

--- app/models/asset.rb
+++ app/models/asset.rb
@@ -1,7 +1,7 @@
 class Asset < ActiveRecord::Base
   belongs_to :recipe
   has_attached_file :image,
-                    styles: { medium: '300x300>', thumb: '100x100>' },
+                    styles: { medium: '300x300#', thumb: '100x100#' },
                     default_url: ':style/no_image_available.jpg'
   validates_attachment_content_type :image, content_type: %r{\Aimage/}
   validates_attachment_file_name :image, matches: [/png\Z/, /jpe?g\Z/]
